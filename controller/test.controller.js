96f97fc1f14afe45e4632dfb445ce187a741da05
Saeed <sa.ghofraniivari@gmail.com>
2022-04-18 11:40:14 +0430

    controller done

--- controller/test.controller.js
+++ controller/test.controller.js
@@ -1,7 +1,17 @@
 const logParam = (request, response, _next) => {
 
-    console.log(request.body); 
-    return response.json({ path: request.originalUrl, success: true, result: request.body });
+    console.log(request.body);
+    console.log(response.locals.message);
+    
+    return response.json({
+        path: request.originalUrl,
+        success: true,
+        result: {
+            id: request.body.data,
+            message: response.locals.message,
+            input: request.body.input
+        }
+    });
 
 };
 
